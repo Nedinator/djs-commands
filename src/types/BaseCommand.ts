e086c19726df782f6f2d62b5b1a87aca75375060
Nedinator <imnedinator@gmail.com>
2026-03-12 21:55:03 -0500

    Fix: added support for slash command options

--- src/types/BaseCommand.ts
+++ src/types/BaseCommand.ts
@@ -1,9 +1,17 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import {
+  ChatInputCommandInteraction,
+  SlashCommandBuilder,
+  SlashCommandOptionsOnlyBuilder,
+  SlashCommandSubcommandsOnlyBuilder,
+} from "discord.js";
 
 export interface BaseCommand {
   name: string;
-  description?: string;
-  slashCommand: SlashCommandBuilder;
+  description: string;
+  slashCommand:
+    | SlashCommandBuilder
+    | SlashCommandOptionsOnlyBuilder
+    | SlashCommandSubcommandsOnlyBuilder;
 
   run(interaction: ChatInputCommandInteraction): Promise<void>;
 }
